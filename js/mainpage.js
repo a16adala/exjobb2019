636f3d7af3a41afcb65e40a36d447fb9f80986ef
Adam Lämmel <37834994+a16adala@users.noreply.github.com>
2019-03-26 05:36:23 +0100

    More functionality and added a basic game layout

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,8 +1,7 @@
+var gameorder = 1;
+
 function startgame() {
-    $(".imglarge").hide();
-    $(".longtext").hide();
-    $(".bigtext").hide();
-    $("#textheader").hide();
+    $("#firstpage").hide();
     $("#secondarybody").show()
     document.title = "Instruktioner";
 }
@@ -10,4 +9,60 @@ function startgame() {
 function pagedefault() {
     document.title = "Välkommen";
     $("#secondarybody").hide();
+    $('#medgrafikpage').hide();
+    $('.speldiven').hide();
+}
+
+function savename() {
+    var username = document.getElementById("saveusername").value;
+    var nameok = confirm("Är du säker på att du vill heta " + username + " ?");
+    if (nameok == true ) {
+        localStorage.setItem("username", username);
+    }
+}
+
+function printname() {
+    document.getElementById("username").innerHTML = localStorage.getItem("username", username);
+}
+
+function gameshuffler() {
+    //gameorder = Math.floor((Math.random() * 2) + 1);
+    //gameorder = 1;
+    if (gameorder == 1) {
+        var gameshuffle = '<img src="pictures/medgrafisk.png" id="imgleft" onclick="clickgame()">' +
+                '<img src="pictures/arrow.png" id="imgcentre">' +
+                '<img src="pictures/utangrafisk.png" id="imgright">;';
+        document.getElementById("gameholder").innerHTML = gameshuffle;
+    }
+    else {
+         var gameshuffle = '<img src="pictures/utangrafisk.png" id="imgleft" onclick="clickgame()">' +
+                '<img src="pictures/arrow.png" id="imgcentre">' +
+                '<img src="pictures/medgrafisk.png" id="imgright">;';
+        document.getElementById("gameholder").innerHTML = gameshuffle;
+    }
+}
+
+function hidemainpage() {
+    $(".imglarge").hide();
+    $(".longtext").hide();
+    $(".bigtext").hide();
+    $("#textheader").hide();
+    $("#secondarybody").hide()
+}
+
+function correctgame() {
+    if (gameorder == 1) {
+        document.title = 'Med grafisk feedback';
+        /* var gamepreview = '';
+        document.getElementById("gamepreview").innerHTML = gamepreview;*/
+        $('#medgrafikpage').show()
+        $('#utangrafikpage').hide()
+    }
+    else {
+        document.title = 'Utan grafisk feedback';
+        /*var gamepreview = '';
+        document.getElementById("gamepreview").innerHTML = gamepreview;*/
+        $('#medgrafikpage').hide()
+        $('#utangrafikpage').show()
+    }
 }
--- js/medgrafisk.js
+++ js/medgrafisk.js
@@ -1,4 +1,32 @@
-function medgrafik() {
-    console.log('medgrafik');
-    $('#medgrafikpage').show();
+/*----- spel-funktionalitet -----------*/
+
+var ctx;
+
+document.addEventListener('DOMContentLoaded',initcanvas,false);
+function initcanvas() {
+    var canvas = document.getElementById("grafikcanvas");
+    ctx = canvas.getContext("2d");
+    ctx.strokeStyle = "#FF0000";
+    ctx.strokeRect(0,0,500,500);
+    for(i=0;i<500;i+=100){
+        ctx.beginPath();
+        ctx.moveTo(0,i);
+        ctx.lineTo(500,i);
+        ctx.moveTo(i,0);
+        ctx.lineTo(i,500);
+        ctx.stroke();
+    }
 }
+
+/*----- sid-funktionalitet -----------*/
+
+function medgrafik() {
+    hidemainpage();
+    console.log('medgrafik');
+    $('#medgrafikpage').hide();
+    $('.footerheader').hide();
+    $('.progressbar').hide();
+    $('.textspel').hide();
+}
+
+
