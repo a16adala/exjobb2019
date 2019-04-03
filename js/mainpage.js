ac094b682a48963dc02d6699e4a8991f7a54f95f
Adam Lämmel <37834994+a16adala@users.noreply.github.com>
2019-04-04 01:47:17 +0200

    updates

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,4 +1,6 @@
-var gameorder = 1;
+var gameorder;
+var username;
+var savedname = 0;
 
 function startgame() {
     $("#firstpage").hide();
@@ -10,23 +12,27 @@ function pagedefault() {
     document.title = "Välkommen";
     $("#secondarybody").hide();
     $('#medgrafikpage').hide();
-    $('.speldiven').hide();
+    $('#utangrafikpage').hide();
+    //$('.speldiven').hide();
 }
 
 function savename() {
-    var username = document.getElementById("saveusername").value;
+    username = document.getElementById("saveusername").value;
     var nameok = confirm("Är du säker på att du vill heta " + username + " ?");
     if (nameok == true ) {
         localStorage.setItem("username", username);
+        savedname = 1;
     }
 }
 
-function printname() {
-    document.getElementById("username").innerHTML = localStorage.getItem("username", username);
+function printstats() {
+    document.getElementById("username").innerHTML = '<h2>Namn:</h2> ' +  '<h3>' + localStorage.getItem("username", username) + '</h3>';
+    document.getElementById("tidmedgrafik").innerHTML = '<h2>Tid på spelet med grafik:</h2> ';
+    document.getElementById("tidutangrafik").innerHTML = '<h2>Tid på spelet utan grafik:</h2> ';
 }
 
 function gameshuffler() {
-    //gameorder = Math.floor((Math.random() * 2) + 1);
+    gameorder = Math.floor((Math.random() * 2) + 1);
     //gameorder = 1;
     if (gameorder == 1) {
         var gameshuffle = '<img src="pictures/medgrafisk.png" id="imgleft" onclick="clickgame()">' +
@@ -43,26 +49,46 @@ function gameshuffler() {
 }
 
 function hidemainpage() {
-    $(".imglarge").hide();
-    $(".longtext").hide();
-    $(".bigtext").hide();
-    $("#textheader").hide();
-    $("#secondarybody").hide()
+    if (savedname == 0) {
+        alert('Du har inte fyllt i ditt namn');
+    }
+    else {
+        correctgame();
+        $(".imglarge").hide();
+        $(".longtext").hide();
+        $(".bigtext").hide();
+        $("#textheader").hide();
+        $("#secondarybody").hide();
+    }
 }
 
 function correctgame() {
+    //$('.speldiven').show();
     if (gameorder == 1) {
+        gameorder == document
         document.title = 'Med grafisk feedback';
         /* var gamepreview = '';
         document.getElementById("gamepreview").innerHTML = gamepreview;*/
-        $('#medgrafikpage').show()
-        $('#utangrafikpage').hide()
+        $('#medgrafikpage').show();
+        $('#utangrafikpage').hide();
     }
     else {
         document.title = 'Utan grafisk feedback';
         /*var gamepreview = '';
         document.getElementById("gamepreview").innerHTML = gamepreview;*/
-        $('#medgrafikpage').hide()
-        $('#utangrafikpage').show()
+        $('#medgrafikpage').hide();
+        $('#utangrafikpage').show();
     }
+}
+
+function medgrafik() {
+    console.log('medgrafik');
+    var mgrafik = '<a href ="medgrafiklvl1.html">' + '<h1>Gå vidare</h1>' + '</a>';
+    document.getElementById("vidare3").innerHTML = mgrafik;
+}
+
+function utangrafik() {
+    console.log('utangrafik');
+    var mgrafik = '<a href ="utangrafiklvl1.html">' + '<h1>Gå vidare</h1>' + '</a>';
+    document.getElementById("vidare4").innerHTML = mgrafik;
 }
